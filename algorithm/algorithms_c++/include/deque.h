e57f55be120894fd94a0d4d068e00d58fcb73425
zhuyouze <563541918@qq.com>
2015-01-30 21:38:48 +0800

    change deque

--- algorithm/algorithms_c++/include/deque.h
+++ algorithm/algorithms_c++/include/deque.h
@@ -13,21 +13,21 @@ struct deque_operate
 	void    (*clear)              (struct deque_struct *deque);
 	
 	void    (*init_head)          (struct deque_struct *deque,int n);
-	void    (*copy)               (struct deque_struct *deque,struct deque_struct *old);
+	void    (*copy)               (struct deque_struct *deque,struct deque_struct old);
 	void    (*init_n)             (struct deque_struct *deque,int n);
 	void    (*init_repeated)      (struct deque_struct *deque,int n,void *elem);
 	void    (*init_scope)         (struct deque_struct *deque,int beg,int end);
 	void    (*destruct)           (struct deque_struct *deque);
 	
-	int     (*empty)              (struct deque_struct deque);
+	int     (*empty)              (struct deque_struct *deque);
 	struct  list_head *(*begin)   (struct deque_struct deque);
 	struct  list_head *(*end)     (struct deque_struct deque);
 	struct  list_head *(*erase_pos)(struct deque_struct *deque,int pos);
 	struct  list_head *(*erase_scope)(struct deque_struct *deque,int beg,int end);
 	
-	int     (*insert_pos)         (struct deque_struct *deque,int pos,void *elem);
+	struct  list_head *(*insert_pos) (struct deque_struct *deque,int pos,void *elem);
 	void    (*insert_pos_repeated)(struct deque_struct *deque,int pos,int n,void *elem);
-	void    (*insert_pos_scope)   (struct deque_struct *deque,int pos,int beg,void *end);
+	void    (*insert_pos_scope)   (struct deque_struct *deque,int pos,int beg,int end);
 	
 	int     (*max_size)           ();
 	void    (*pop_back)           (struct deque_struct *deque);
@@ -36,7 +36,7 @@ struct deque_operate
 	void    (*push_front)         (struct deque_struct *deque,void *elem);
 	
 	void    (*resize)             (struct deque_struct *deque,int num);
-	void    (*size)               (struct deque_struct *deque);
+	int     (*size)               (struct deque_struct deque);
 	
 	void    (*swap)               (struct deque_struct *deque,struct deque_struct *old);
 };
@@ -54,7 +54,7 @@ void _init(struct deque_struct *h,int num)
 {
 	if(!head) h=(struct deque_struct *)malloc(sizeof(struct deque_struct));
 	INIT_LIST_HEAD(&(h->list));
-	h->head-h;
+	h->head=h;
 	int i;
 	for(i=0;i<num;i++){
 		struct deque_struct *n=(struct deque_struct *)malloc(sizeof(struct deque_struct));
@@ -79,10 +79,9 @@ void assign_scope(struct deque_struct *h,int beg,int end)
 #define assign_repeated(h,n,elem,type) ({ \
 	_init(h,n);                    \
 	struct deque_struct *pos;        \
-	list_for_each_entry(pos, h->head, list){    \
-		pos->data=(type *)malloc(sizeof(type)); \
-		*(pos->data)=(type)(*(elem));           \
-		i++;                                    \
+	list_for_each_entry(pos, (h)->head, list){    \
+		pos->data=malloc(sizeof(type)); \
+		*(pos->data)=*((void *)(elem));         \
 	}                                   })                                         
                                               
 void *at(struct deque_struct h,int idx)
@@ -105,16 +104,112 @@ void *front(struct deque_struct h)
 {
 	return list_entry((h.head)->next,struct deque_struct,list)->data;
 }
-/*Attention if CLEAR all memory should alloc again !*/
+/*Attention! if CLEAR all memory should alloc again(use _init) !*/
 void clear(struct deque_struct *h)
 {
 	struct deque_struct *pos;
 	list_for_each_entry(pos, h->head, list){
-		
+		free(list_entry(pos->list.prev,struct deque_struct,list);
 	}
-	
 }
-#
+
+int size(struct deque_struct h)
+{
+	struct deque_struct *pos;
+	int ret=0;
+	list_for_each_entry(pos, h.head, list){
+		ret++;
+	}
+	return ret;
+}
+#define copy(h,old,type) ({                                       \
+	_init((h),size(old));                                           \
+	struct deque_struct tmp=old;                                  \
+	list_for_each_entry(pos, (h)->head, list){                      \
+		tmp=*(list_entry(tmp.list.next, struct deque_struct, list));  \
+		h->data=malloc(sizeof(type));                          \
+		*(h->data)=*((void *)(tmp.data))                               \
+	}                    })                      
+
+
+void init_n(struct deque_struct *h,int n)
+{
+	_init(h,n);
+}
+
+int empty(struct deque_struct *h)
+{
+	if(list_entry(h->list.next, struct deque_struct, list)==h)
+		return 1;
+	else return 0;
+}
+
+struct  list_head *begin(struct deque_struct h)
+{
+	return h.list.next;
+}
+
+struct  list_head *end(struct deque_struct h)
+{
+	return h.list.prev;
+}
+struct  list_head *erase_pos(struct deque_struct *h,int p)
+{
+	struct deque_struct *pos,save;
+	list_for_each_entry(pos, h->head, list){
+		p--;
+		if(p<0){
+			save=*pos;
+			list_del(&pos->list)
+			break;
+		}
+	}
+	return save.list.next;
+}
+
+struct list_head *erase_scope(struct deque_struct *h,int beg,int end)
+{
+	int count=0;
+	struct deque_struct *pos,save;
+	list_for_each_entry(pos, h->head, list){
+		if(count>=beg&&count<end)
+			list_del(&pos->list);					
+		if(count==end){
+			save=*pos;	
+			break;
+		}
+		count++;
+	}
+	return save->next;
+}
+
+#define insert_pos(h,p,elem,type)  ({           \
+	struct deque_struct *pos;                   \
+	list_for_each_entry(pos, h->head, list){     \
+		p--;                                    \
+		if(p<0) break;                          \
+	}                                           \
+	struct deque_struct *n=(struct deque_struct *)malloc(sizeof(struct deque_struct)); \
+	n->data=malloc(sizeof(type));       \
+	*(n->data)=*((void *)(elem));               \
+	__list_add(&n->list,pos->list.prev,&pos->list);   })
+
+
+#define insert_pos_repeated(h,p,n,elem,type,i)   ({	\
+	struct deque_struct *pos;                   \
+	list_for_each_entry(pos, h->head, list){     \
+		p--;                                    \
+		if(p<0) break;                          \
+	}                                           \
+	for(i=0;i<n;i++){                           \
+		struct deque_struct *ne=(struct deque_struct *)malloc(sizeof(struct deque_struct)); \
+		ne->data=malloc(sizeof(type));       \
+		*(ne->data)=*((void *)(elem));               \
+		__list_add(&ne->list,pos->list.prev,&pos->list);     \
+		pos=list_next_entry(pos, list)                       \
+	}                       })
+		                                          
+	
 #endif
 
 
